32a3b76036af8fa1c1b1864d7e8650e5d182d001
Simon Whitlock <simonjwhitlock@gmail.com>
2025-08-11 18:16:21 +0100

    somat

--- components/ball.py
+++ components/ball.py
@@ -10,7 +10,22 @@ class Ball(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.velocity = pygame.Vector2(0, BALL_START_VELOCITY)
         
-        
+    def reflect(self, NV):
+        self.velocity = self.velocity.reflect(pygame.math.Vector2(NV))
+
     def update(self, dt):
         self.rect.center += self.velocity * dt
         
+        if self.rect.left <= 0:
+            self.reflect((1, 0))
+            self.rect.left = 0
+        if self.rect.right >= SCREEN_WIDTH:
+            self.reflect((-1, 0))
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.reflect((0, 1))
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.reflect((0, -1))
+            self.rect.bottom = SCREEN_HEIGHT
+    
